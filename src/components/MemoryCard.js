5ae7d30582250d796dce60a5bb419e1058a8a1c8
Andrew Pham <andrewpham92@gmail.com>
2020-10-26 19:09:09 -0400

    added card flip

--- src/components/MemoryCard.js
+++ src/components/MemoryCard.js
@@ -3,11 +3,36 @@ import './MemoryCard.css';
 
 
 export default class MemoryCard extends React.Component {
-  render() {
-    return (
+  constructor(props){
+    super(props);
+    this.state = {isFlipped: false}
+  }
 
-      <div className='MemoryCard'>
-        <img src="https://www.digitalcrafts.com/img/logo-wrench-white.png" alt="" />
+  clickHandler = () => {
+    this.setState(
+      {isFlipped : !this.state.isFlipped}
+      )
+  }
+  
+
+  render() {
+    const innerClass =  this.state.isFlipped ? 'MemoryCard__inner flipped' : 'MemoryCard__inner'
+
+
+    return (
+      
+      
+      <div className='MemoryCard' onClick={this.clickHandler}>
+        <div className={innerClass}>
+      
+            <div className='MemoryCard__back'>
+              <img src="https://www.digitalcrafts.com/img/logo-wrench-white.png" alt="" />
+            </div>
+            <div className='MemoryCard__front'>
+              ∆
+            </div>
+
+        </div>
       </div>
 
     )
